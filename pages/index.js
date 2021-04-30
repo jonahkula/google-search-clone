dddf5ddd98b5f5c1a9cb78b1691ac3fabf149147
jonahkula <jonahkula@gmail.com>
2021-04-30 14:21:57 -0700

    updated favicon

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
     <div>
       <Head>
         <title>Google Search Clone</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
     </div>
   );
