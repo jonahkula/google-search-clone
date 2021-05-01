d5835084d7ad1ebd102bd014bfd660416038dbca
jonahkula <jonahkula@gmail.com>
2021-05-01 13:16:58 -0700

    finish home page header

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import Avatar from "../components/Avatar";
+import { ViewGridIcon } from "@heroicons/react/solid";
 
 export default function Home() {
   return (
@@ -17,6 +19,8 @@ export default function Home() {
         <div className="flex space-x-4 items-center">
           <p className="link">Gmail</p>
           <p className="link">Images</p>
+          <ViewGridIcon className="h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer" />
+          <Avatar url="/profile.png" />
         </div>
       </header>
 
