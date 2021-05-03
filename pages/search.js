ed8a649ba105ab29dae2ba5ca7063c9ade076964
jonahkula <jonahkula@gmail.com>
2021-05-03 16:58:42 -0700

    display search results

--- pages/search.js
+++ pages/search.js
@@ -1,13 +1,17 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 import Header from "../components/Header";
+import SearchResults from "../components/SearchResults";
 import { API_KEY, CONTEXT_KEY } from "../keys";
 import Response from "../Response";
 
 function Search({ results }) {
+  const router = useRouter();
+
   return (
     <div>
       <Head>
-        <title>Google Search Clone - Search Results</title>
+        <title>Google Search Clone - {router.query.term}</title>
         <link rel="icon" href="/favicon.png" />
       </Head>
 
@@ -15,6 +19,7 @@ function Search({ results }) {
       <Header />
 
       {/* results */}
+      <SearchResults results={results} />
     </div>
   );
 }
@@ -25,12 +30,13 @@ export default Search;
 export async function getServerSideProps(context) {
   // have dummy data be true when testing to avoid exhausting google's api quota of 150 searches per day
   const useDummyData = true;
+  const startIndex = context.query.start || "0";
 
   // get search results from server
   const data = useDummyData
     ? Response
     : await fetch(
-        `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}`
+        `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`
       ).then((response) => response.json());
 
   return {
